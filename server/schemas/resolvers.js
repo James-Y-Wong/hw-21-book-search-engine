2a772cb07d6656dcd76d8ff916bff7a3985d7a30
James-Y-Wong <jamesyeuwong@gmail.com>
2022-07-29 17:27:38 -0400

    complete resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,67 @@
+
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+  Query: {
+    users: async () => {
+      return User.find().populate('books');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('books');
+    },
+    books: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Book.find(params).sort({ createdAt: -1 });
+    },
+    book: async (parent, { bookId }) => {
+      return Book.findOne({ _id: bookId });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+
+      const user = await User.create({ username, email, password });
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { authors, description, title, bookId, image, link }) => {
+      const book = await Book.create({ authors, description, title, bookId, image, link });
+
+      await User.findOneAndUpdate(
+        { email },
+        { $addToSet: { savedBooks: book._id } }
+      );
+
+      return book;
+    },
+    removeBook: async (parent, { bookId }) => {
+      return Book.findOneAndDelete({ _id: bookId });
+    },
+  },
+};
+
+module.exports = resolvers;
